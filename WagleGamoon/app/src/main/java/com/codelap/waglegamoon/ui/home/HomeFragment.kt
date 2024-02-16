1f5ab8d278f4a72e2c42e2e28d35ad110040be41
Tnalxmsk <alstnwkd990@naver.com>
2024-02-17 05:50:40 +0900

    feat: 홈 화면 Ui 변경

--- WagleGamoon/app/src/main/java/com/codelap/waglegamoon/ui/home/HomeFragment.kt
+++ WagleGamoon/app/src/main/java/com/codelap/waglegamoon/ui/home/HomeFragment.kt
@@ -15,11 +15,11 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
     private fun setClickListener() {
         with(binding) {
-            button5.setOnClickListener {
+            btnFreePostButton.setOnClickListener {
                 val action = HomeFragmentDirections.actionHomeToHomeFreePost()
                 findNavController().navigate(action)
             }
-            button6.setOnClickListener {
+            btnQuestionPostButton.setOnClickListener {
                 val action = HomeFragmentDirections.actionHomeToHomeQuestionPost()
                 findNavController().navigate(action)
             }
