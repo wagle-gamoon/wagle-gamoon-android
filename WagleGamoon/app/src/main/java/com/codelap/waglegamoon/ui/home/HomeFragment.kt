1fe9a6f8adb8873afc2bc1f54191fb3a2566e977
kkemman <111038147+kkemman@users.noreply.github.com>
2024-02-17 05:23:07 +0900

    bkg

--- WagleGamoon/app/src/main/java/com/codelap/waglegamoon/ui/home/HomeFragment.kt
+++ WagleGamoon/app/src/main/java/com/codelap/waglegamoon/ui/home/HomeFragment.kt
@@ -1,11 +1,27 @@
 package com.codelap.waglegamoon.ui.home
 
+import androidx.lifecycle.findViewTreeViewModelStoreOwner
+import androidx.navigation.fragment.findNavController
 import com.codelap.waglegamoon.R
 import com.codelap.waglegamoon.base.BaseFragment
 import com.codelap.waglegamoon.databinding.FragmentHomeBinding
 
+
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     override fun setLayout() {
+        setClickListener()
+    }
 
+    private fun setClickListener() {
+        with(binding) {
+            button5.setOnClickListener {
+                val action = HomeFragmentDirections.actionHomeToHomeFreePost()
+                findNavController().navigate(action)
+            }
+            button6.setOnClickListener {
+                val action = HomeFragmentDirections.actionHomeToHomeQuestionPost()
+                findNavController().navigate(action)
+            }
+        }
     }
 }
