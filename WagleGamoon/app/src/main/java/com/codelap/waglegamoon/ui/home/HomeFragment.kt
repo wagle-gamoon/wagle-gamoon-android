00a482738939068a06bc6b2ee924cd18240d11fe
Tnalxmsk <alstnwkd990@naver.com>
2024-02-17 04:02:45 +0900

    feat: 스터디 모집 화면 더미 데이터 구현

--- WagleGamoon/app/src/main/java/com/codelap/waglegamoon/ui/home/HomeFragment.kt
+++ WagleGamoon/app/src/main/java/com/codelap/waglegamoon/ui/home/HomeFragment.kt
@@ -1,11 +1,13 @@
 package com.codelap.waglegamoon.ui.home
 
+import androidx.navigation.fragment.findNavController
 import com.codelap.waglegamoon.R
 import com.codelap.waglegamoon.base.BaseFragment
 import com.codelap.waglegamoon.databinding.FragmentHomeBinding
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     override fun setLayout() {
-
+        val action = HomeFragmentDirections.actionNavigationHomeToNavigationHomeStudyRecruit()
+        findNavController().navigate(action)
     }
 }
