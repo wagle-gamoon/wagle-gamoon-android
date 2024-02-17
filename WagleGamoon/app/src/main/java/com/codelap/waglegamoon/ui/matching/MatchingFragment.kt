2d0e9c76ea4705b76ab1e3cea0fc95e40f315277
Tnalxmsk <alstnwkd990@naver.com>
2024-02-17 09:55:22 +0900

    feat: 가위바위보 선택 구현

--- WagleGamoon/app/src/main/java/com/codelap/waglegamoon/ui/matching/MatchingFragment.kt
+++ WagleGamoon/app/src/main/java/com/codelap/waglegamoon/ui/matching/MatchingFragment.kt
@@ -1,10 +1,21 @@
 package com.codelap.waglegamoon.ui.matching
 
+import androidx.navigation.fragment.findNavController
 import com.codelap.waglegamoon.R
 import com.codelap.waglegamoon.base.BaseFragment
 import com.codelap.waglegamoon.databinding.FragmentMatchingBinding
 
 class MatchingFragment : BaseFragment<FragmentMatchingBinding>(R.layout.fragment_matching) {
     override fun setLayout() {
+        setClickListener()
+    }
+
+    private fun setClickListener() {
+        with(binding) {
+            btnMatchingSuccess.setOnClickListener {
+                val action = MatchingFragmentDirections.actionNavigationMatchingToNavigationMatchingRockPaperScissorsFragmen()
+                findNavController().navigate(action)
+            }
+        }
     }
 }
